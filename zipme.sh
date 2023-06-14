6d0dbfd69dc6f03dacd3cf094bec0297a55243cc
Jamie Jones <yagisan@yagisan.org>
2023-06-15 02:03:28 +0900

    Update zipme.sh

--- zipme.sh
+++ zipme.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-git archive --format=zip -9 -v --output=yagisans_better_stellaris.zip main
+git archive --format=zip -9 -v --add-file=yagisans_better_stellaris.mod --prefix=yagisans_better_stellaris/ --output=yagisans_better_stellaris.zip main
